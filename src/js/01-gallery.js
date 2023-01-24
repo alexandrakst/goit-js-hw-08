8ce344f30522d73531b6222770303d4d4050241d
alexandrakst <alexandra.kostenko@hotmail.com>
2023-01-23 21:02:22 -0500

    remove comments

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,8 +1,8 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Описаний в документації
+
 import SimpleLightbox from 'simplelightbox';
-// Додатковий імпорт стилів
+
 import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
