6c32ee360032d47779e9fe557dbc793aa3c897cb
alexandrakst <alexandra.kostenko@hotmail.com>
2023-01-23 22:36:24 -0500

    task 2 update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const VIDEOPLAYER_CURRENT_TIME = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    const time = data.seconds;
+    console.log(time);
+
+    localStorage.setItem(VIDEOPLAYER_CURRENT_TIME, time);
+  }, 1000)
+);
+
+setCurrentTime();
+function setCurrentTime() {
+  if (!localStorage.getItem(VIDEOPLAYER_CURRENT_TIME)) {
+    return;
+  }
+  player.setCurrentTime(localStorage.getItem(VIDEOPLAYER_CURRENT_TIME));
+}
